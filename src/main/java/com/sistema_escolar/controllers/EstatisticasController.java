b3a3dc8bfae747bc10b2ed11277bb4e73ca8cefd
Omar <164200540+omaraljawabri@users.noreply.github.com>
2024-12-26 19:48:57 -0300

    Documentação da classe EstatisticasController

--- src/main/java/com/sistema_escolar/controllers/EstatisticasController.java
+++ src/main/java/com/sistema_escolar/controllers/EstatisticasController.java
@@ -5,7 +5,11 @@ import com.sistema_escolar.dtos.response.EstatisticasGeraisResponseDTO;
 import com.sistema_escolar.dtos.response.EstatisticasTurmaResponseDTO;
 import com.sistema_escolar.entities.Usuario;
 import com.sistema_escolar.services.EstatisticasService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -19,10 +23,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/estatisticas")
 @RequiredArgsConstructor
 @SecurityRequirement(name = "securityConfig")
+@Tag(description = "Endpoints responsáveis por prover estatísticas ligadas a notas, médias, quantidade de turmas, etc",
+        name = "Estatísticas")
 public class EstatisticasController {
 
     private final EstatisticasService estatisticasService;
 
+    @Operation(summary = "Endpoint responsável por buscar estatísticas relacionadas ao desempenho de uma turma pelo seu id",
+            description = "Necessita da ROLE_PROFESSOR para ser acessado",
+            method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Operação realizada com sucesso!"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "401", description = "Usuário não foi autorizado"),
+            @ApiResponse(responseCode = "403", description = "Usuário não tem permissão necessária para realizar operação"),
+            @ApiResponse(responseCode = "500", description = "Erro ao realizar operação(Internal server error)")
+    })
     @GetMapping("/turma/{id}")
     public ResponseEntity<EstatisticasTurmaResponseDTO> estatisticasDaTurma(@PathVariable Long id){
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -30,6 +46,16 @@ public class EstatisticasController {
         return ResponseEntity.ok(estatisticasService.estatisticasDaTurma(id, usuario));
     }
 
+    @Operation(summary = "Endpoint responsável por buscar estatísticas relacionadas ao desempenho de um estudante",
+            description = "Necessita da ROLE_ESTUDANTE para ser acessado",
+            method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Operação realizada com sucesso!"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "401", description = "Usuário não foi autorizado"),
+            @ApiResponse(responseCode = "403", description = "Usuário não tem permissão necessária para realizar operação"),
+            @ApiResponse(responseCode = "500", description = "Erro ao realizar operação(Internal server error)")
+    })
     @GetMapping("/estudante")
     public ResponseEntity<EstatisticasEstudanteResponseDTO> estatisticasDoEstudante(){
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -37,6 +63,16 @@ public class EstatisticasController {
         return ResponseEntity.ok(estatisticasService.estatisticasDoEstudante(usuario));
     }
 
+    @Operation(summary = "Endpoint responsável por buscar estatísticas gerais do sistema, como número de disciplinas e turmas",
+            description = "Necessita da ROLE_ADMIN para ser acessado",
+            method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Operação realizada com sucesso!"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "401", description = "Usuário não foi autorizado"),
+            @ApiResponse(responseCode = "403", description = "Usuário não tem permissão necessária para realizar operação"),
+            @ApiResponse(responseCode = "500", description = "Erro ao realizar operação(Internal server error)")
+    })
     @GetMapping("/geral")
     public ResponseEntity<EstatisticasGeraisResponseDTO> estatisticasGerais(){
         return ResponseEntity.ok(estatisticasService.estatisticasGerais());
