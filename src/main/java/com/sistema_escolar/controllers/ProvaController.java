511cadd994d831516d1a115dd03ad5ea7ae7b551
Omar <164200540+omaraljawabri@users.noreply.github.com>
2024-12-26 23:13:56 -0300

    Documentação da classe ProvaController

--- src/main/java/com/sistema_escolar/controllers/ProvaController.java
+++ src/main/java/com/sistema_escolar/controllers/ProvaController.java
@@ -8,7 +8,11 @@ import com.sistema_escolar.dtos.response.ProvaRespondidaResponseDTO;
 import com.sistema_escolar.dtos.response.ProvaResponseDTO;
 import com.sistema_escolar.entities.Usuario;
 import com.sistema_escolar.services.ProvaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -21,10 +25,21 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/prova")
 @RequiredArgsConstructor
 @SecurityRequirement(name = "securityConfig")
+@Tag(description = "Endpoints responsáveis por operações ligadas a prova", name = "Provas")
 public class ProvaController {
 
     private final ProvaService provaService;
 
+    @Operation(summary = "Endpoint responsável por criar uma prova e suas questões",
+            description = "Necessita da ROLE_PROFESSOR para ser acessado",
+            method = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Operação realizada com sucesso!"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "401", description = "Usuário não foi autorizado"),
+            @ApiResponse(responseCode = "403", description = "Usuário não tem permissão necessária para realizar operação"),
+            @ApiResponse(responseCode = "500", description = "Erro ao realizar operação(Internal server error)")
+    })
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ProvaResponseDTO> createProva(@RequestBody ProvaPostRequestDTO provaPostRequestDTO){
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -32,14 +47,34 @@ public class ProvaController {
         return ResponseEntity.ok(provaService.createProva(provaPostRequestDTO, usuario));
     }
 
-    @PutMapping("/{id}")
+    @Operation(summary = "Endpoint responsável por atualizar uma prova e suas questões",
+            description = "Necessita da ROLE_PROFESSOR para ser acessado",
+            method = "PUT")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Operação realizada com sucesso!"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "401", description = "Usuário não foi autorizado"),
+            @ApiResponse(responseCode = "403", description = "Usuário não tem permissão necessária para realizar operação"),
+            @ApiResponse(responseCode = "500", description = "Erro ao realizar operação(Internal server error)")
+    })
+    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ProvaResponseDTO> updateProva(@PathVariable Long id, @RequestBody ProvaPutRequestDTO provaPutRequestDTO){
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         Usuario usuario = (Usuario) authentication.getPrincipal();
         return ResponseEntity.ok(provaService.updateProva(id, provaPutRequestDTO, usuario));
     }
 
-    @PostMapping("/publish/{id}")
+    @Operation(summary = "Endpoint responsável por publicar uma prova pelo seu id, passando o tempo até expirar",
+            description = "Necessita da ROLE_PROFESSOR para ser acessado",
+            method = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Operação realizada com sucesso!"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "401", description = "Usuário não foi autorizado"),
+            @ApiResponse(responseCode = "403", description = "Usuário não tem permissão necessária para realizar operação"),
+            @ApiResponse(responseCode = "500", description = "Erro ao realizar operação(Internal server error)")
+    })
+    @PostMapping(value = "/publish/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> publishProva(@PathVariable Long id, @RequestBody PublishProvaRequestDTO publishProvaRequestDTO){
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         Usuario usuario = (Usuario) authentication.getPrincipal();
@@ -47,7 +82,17 @@ public class ProvaController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @GetMapping("/avaliada/{id}")
+    @Operation(summary = "Endpoint responsável por buscar uma prova do estudante já avaliada pelo seu id",
+            description = "Necessita da ROLE_ESTUDANTE para ser acessado",
+            method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Operação realizada com sucesso!"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "401", description = "Usuário não foi autorizado"),
+            @ApiResponse(responseCode = "403", description = "Usuário não tem permissão necessária para realizar operação"),
+            @ApiResponse(responseCode = "500", description = "Erro ao realizar operação(Internal server error)")
+    })
+    @GetMapping(value = "/avaliada/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ProvaAvaliadaResponseDTO> getProvaAvaliada(@PathVariable Long id){
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         Usuario usuario = (Usuario) authentication.getPrincipal();
